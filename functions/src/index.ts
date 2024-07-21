7996fad1bf31b6d60c30bb9c1bb79f3021a5bb44
Anthony Nardi <anthonygt@gmail.com>
2024-07-21 09:51:41 -0400

    ts function requests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,10 @@
 import admin from "firebase-admin";
 
-import { onCall, HttpsError } from "firebase-functions/v2/https";
+import {
+  onCall,
+  HttpsError,
+  CallableRequest,
+} from "firebase-functions/v2/https";
 import { logger } from "firebase-functions/v2";
 import { getDatabase } from "firebase-admin/database";
 import {
@@ -28,170 +32,183 @@ import { Notification } from "./types";
 
 admin.initializeApp();
 
-exports.bid = onCall(async (request) => {
-  verifyRequestAuthentication(request);
+exports.bid = onCall(
+  async (
+    request: CallableRequest<{
+      lobbyUID: string;
+      bid: string[];
+    }>
+  ) => {
+    verifyRequestAuthentication(request);
 
-  const { lobbyUID, bid } = request.data;
-  const requestEmail = getEmailFromRequest(request);
-  const gameState = await getGameState(lobbyUID);
+    const { lobbyUID, bid } = request.data;
+    const requestEmail = getEmailFromRequest(request);
+    const gameState = await getGameState(lobbyUID);
 
-  if (!isActivePlayerTakingAction(gameState, requestEmail)) {
-    return;
-  }
-
-  const activePlayer = getActivePlayer(gameState);
-
-  updatePlayerLastAction(activePlayer);
-  updatePlayersBid(activePlayer, bid);
-  updateNextActivePlayer(gameState);
-
-  const totalBid = bid.reduce(
-    (sum: number, current: number) => Number(sum) + Number(current),
-    0
-  );
-
-  const notification: Notification = {
-    timestamp: Date.now(),
-    title: `${requestEmail} has highest bid of ${totalBid}.`,
-  };
-
-  gameState.public.notification = notification;
-
-  return updateGameState(
-    gameState,
-    `Player ${requestEmail} bid ${bid.join(",")}`
-  );
-});
-
-exports.passturn = onCall(async (request) => {
-  verifyRequestAuthentication(request);
-
-  const requestEmail = getEmailFromRequest(request);
-  const { lobbyUID } = request.data;
-
-  const gameState = await getGameState(lobbyUID);
-  const players = gameState.public.players;
-
-  if (!isActivePlayerTakingAction(gameState, requestEmail)) {
-    return;
-  }
-
-  const doesBiddingRoundEndOnFirstPass =
-    getDoesBiddingRoundEndOnFirstPass(gameState);
-
-  if (doesBiddingRoundEndOnFirstPass) {
-    // The active player is the one who passed.
-
-    // Award the player who passed the currentStatusCard
-    // The player who passed current bid is returned to their moneyCards
-    // Reset all players current bids
-    // Flip a new card from the deck
-
-    const cardAwarded = gameState.public.currentStatusCard;
-
-    players.forEach((player) => {
-      if (player.email === requestEmail) {
-        updatePlayerLastAction(player);
-        giveCurrentStatusCardToPlayer(player, gameState);
-        returnPlayersBidToHand(player);
-        maybeUseMinusCard(player, gameState);
-      } else {
-        player.currentBid = [];
-      }
-    });
-
-    revealNewStatusCard(gameState);
-
-    let message = "";
-
-    if (isGameOver(gameState)) {
-      gameState.public.status = "GAME_OVER";
-      const { currentStatusCard } = gameState.public;
-      message = `${currentStatusCard} was revealed. Game over.`;
+    if (!isActivePlayerTakingAction(gameState, requestEmail)) {
+      return;
     }
+
+    const activePlayer = getActivePlayer(gameState);
+
+    updatePlayerLastAction(activePlayer);
+    updatePlayersBid(activePlayer, bid);
+    updateNextActivePlayer(gameState);
+
+    const totalBid = bid.reduce(
+      (sum: number, current) => Number(sum) + Number(current),
+      0
+    );
 
     const notification: Notification = {
       timestamp: Date.now(),
-      title: `${requestEmail} passes and receives ${cardAwarded}.`,
-      message,
+      title: `${requestEmail} has highest bid of ${totalBid}.`,
     };
 
     gameState.public.notification = notification;
 
     return updateGameState(
       gameState,
-      `${requestEmail} is the first to pass and receives ${cardAwarded}.`
+      `Player ${requestEmail} bid ${bid.join(",")}`
     );
   }
+);
 
-  const activePlayer = getActivePlayer(gameState);
+exports.passturn = onCall(
+  async (
+    request: CallableRequest<{
+      lobbyUID: string;
+    }>
+  ) => {
+    verifyRequestAuthentication(request);
 
-  updatePlayerLastAction(activePlayer);
-  setActivePlayerPass(gameState);
-  returnPlayersBidToHand(activePlayer);
+    const requestEmail = getEmailFromRequest(request);
+    const { lobbyUID } = request.data;
 
-  const playersWithActiveBids = getPlayersActivelyBidding(gameState);
+    const gameState = await getGameState(lobbyUID);
+    const players = gameState.public.players;
 
-  if (playersWithActiveBids === 1) {
-    // Award the player with the only remaining bid the currentStatusCard
-    // All players who passed return their currentBid to their moneyCard
-    // Set all players hasPassed flags to false
-    // Flip a new card from the deck
-
-    let auctionWinner = "";
-    let totalBid = 0;
-    const cardAwarded = gameState.public.currentStatusCard;
-
-    players.forEach((player) => {
-      if (player.hasPassed === false) {
-        totalBid = (player.currentBid || []).reduce(
-          (sum: number, current: string) => Number(sum) + Number(current),
-          0
-        );
-
-        awardPlayerWithCurrentStatusCard(player, gameState);
-        auctionWinner = player.email;
-      }
-
-      player.hasPassed = false;
-    });
-
-    revealNewStatusCard(gameState);
-
-    let message = "";
-
-    if (isGameOver(gameState)) {
-      gameState.public.status = "GAME_OVER";
-      const { currentStatusCard } = gameState.public;
-      message = `${currentStatusCard} was revealed. Game over.`;
+    if (!isActivePlayerTakingAction(gameState, requestEmail)) {
+      return;
     }
 
-    gameState.public.activePlayer = auctionWinner;
+    const doesBiddingRoundEndOnFirstPass =
+      getDoesBiddingRoundEndOnFirstPass(gameState);
 
-    const notification: Notification = {
-      timestamp: Date.now(),
-      title: `${auctionWinner} won ${cardAwarded} for a total of ${totalBid}.`,
-      message,
-    };
+    if (doesBiddingRoundEndOnFirstPass) {
+      // The active player is the one who passed.
 
-    gameState.public.notification = notification;
+      // Award the player who passed the currentStatusCard
+      // The player who passed current bid is returned to their moneyCards
+      // Reset all players current bids
+      // Flip a new card from the deck
 
-    return updateGameState(gameState, `Player ${auctionWinner} won auction.`);
-  } else {
-    // Update the next player
-    // Player who passed has their bid returned to their hand
-    updateNextActivePlayer(gameState);
+      const cardAwarded = gameState.public.currentStatusCard;
 
-    const notification: Notification = {
-      timestamp: Date.now(),
-      title: `${requestEmail} passed.`,
-    };
+      players.forEach((player) => {
+        if (player.email === requestEmail) {
+          updatePlayerLastAction(player);
+          giveCurrentStatusCardToPlayer(player, gameState);
+          returnPlayersBidToHand(player);
+          maybeUseMinusCard(player, gameState);
+        } else {
+          player.currentBid = [];
+        }
+      });
 
-    gameState.public.notification = notification;
+      revealNewStatusCard(gameState);
 
-    return updateGameState(gameState, `${requestEmail} has passed.`);
+      let message = "";
+
+      if (isGameOver(gameState)) {
+        gameState.public.status = "GAME_OVER";
+        const { currentStatusCard } = gameState.public;
+        message = `${currentStatusCard} was revealed. Game over.`;
+      }
+
+      const notification: Notification = {
+        timestamp: Date.now(),
+        title: `${requestEmail} passes and receives ${cardAwarded}.`,
+        message,
+      };
+
+      gameState.public.notification = notification;
+
+      return updateGameState(
+        gameState,
+        `${requestEmail} is the first to pass and receives ${cardAwarded}.`
+      );
+    }
+
+    const activePlayer = getActivePlayer(gameState);
+
+    updatePlayerLastAction(activePlayer);
+    setActivePlayerPass(gameState);
+    returnPlayersBidToHand(activePlayer);
+
+    const playersWithActiveBids = getPlayersActivelyBidding(gameState);
+
+    if (playersWithActiveBids === 1) {
+      // Award the player with the only remaining bid the currentStatusCard
+      // All players who passed return their currentBid to their moneyCard
+      // Set all players hasPassed flags to false
+      // Flip a new card from the deck
+
+      let auctionWinner = "";
+      let totalBid = 0;
+      const cardAwarded = gameState.public.currentStatusCard;
+
+      players.forEach((player) => {
+        if (player.hasPassed === false) {
+          totalBid = (player.currentBid || []).reduce(
+            (sum: number, current: string) => Number(sum) + Number(current),
+            0
+          );
+
+          awardPlayerWithCurrentStatusCard(player, gameState);
+          auctionWinner = player.email;
+        }
+
+        player.hasPassed = false;
+      });
+
+      revealNewStatusCard(gameState);
+
+      let message = "";
+
+      if (isGameOver(gameState)) {
+        gameState.public.status = "GAME_OVER";
+        const { currentStatusCard } = gameState.public;
+        message = `${currentStatusCard} was revealed. Game over.`;
+      }
+
+      gameState.public.activePlayer = auctionWinner;
+
+      const notification: Notification = {
+        timestamp: Date.now(),
+        title: `${auctionWinner} won ${cardAwarded} for a total of ${totalBid}.`,
+        message,
+      };
+
+      gameState.public.notification = notification;
+
+      return updateGameState(gameState, `Player ${auctionWinner} won auction.`);
+    } else {
+      // Update the next player
+      // Player who passed has their bid returned to their hand
+      updateNextActivePlayer(gameState);
+
+      const notification: Notification = {
+        timestamp: Date.now(),
+        title: `${requestEmail} passed.`,
+      };
+
+      gameState.public.notification = notification;
+
+      return updateGameState(gameState, `${requestEmail} has passed.`);
+    }
   }
-});
+);
 
 exports.createlobby = onCall((request) => {
   verifyRequestAuthentication(request);
@@ -222,116 +239,134 @@ exports.createlobby = onCall((request) => {
     });
 });
 
-exports.joinlobby = onCall(async (request) => {
-  // Checking that the user is authenticated.
-  if (!request.auth) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new HttpsError(
-      "failed-precondition",
-      "The function must be " + "called while authenticated."
+exports.joinlobby = onCall(
+  async (
+    request: CallableRequest<{
+      lobbyUID: string;
+      email: string;
+    }>
+  ) => {
+    // Checking that the user is authenticated.
+    if (!request.auth) {
+      // Throwing an HttpsError so that the client gets the error details.
+      throw new HttpsError(
+        "failed-precondition",
+        "The function must be " + "called while authenticated."
+      );
+    }
+
+    const { lobbyUID, email } = request.data;
+
+    // Add new player to the lobby if they don't already exist
+
+    const players = await getDatabase()
+      .ref(`lobbies/${lobbyUID}/players`)
+      .get();
+
+    const playersSnapshotValue = players.val();
+
+    console.log(playersSnapshotValue);
+
+    if (playersSnapshotValue.length >= 5) {
+      return;
+    }
+
+    const gameState = await getGameState(lobbyUID);
+
+    if (gameState) {
+      return;
+    }
+
+    const doesPlayerExist = playersSnapshotValue.find(
+      (player: any) => player.email === email
     );
-  }
 
-  const { lobbyUID, email } = request.data;
+    console.log("doesPlayerExist:", doesPlayerExist ? "true" : "false");
 
-  // Add new player to the lobby if they don't already exist
+    if (doesPlayerExist) return;
 
-  const players = await getDatabase().ref(`lobbies/${lobbyUID}/players`).get();
+    console.log(`Attempting to add ${email} to the lobby.`);
 
-  const playersSnapshotValue = players.val();
-
-  console.log(playersSnapshotValue);
-
-  if (playersSnapshotValue.length >= 5) {
-    return;
-  }
-
-  const gameState = await getGameState(lobbyUID);
-
-  if (gameState) {
-    return;
-  }
-
-  const doesPlayerExist = playersSnapshotValue.find(
-    (player: any) => player.email === email
-  );
-
-  console.log("doesPlayerExist:", doesPlayerExist ? "true" : "false");
-
-  if (doesPlayerExist) return;
-
-  console.log(`Attempting to add ${email} to the lobby.`);
-
-  playersSnapshotValue.push({
-    email,
-    ready: false,
-    joinedAt: Date.now().toString(),
-  });
-
-  console.log(`New players: ${playersSnapshotValue}`);
-
-  getDatabase()
-    .ref(`lobbies/${lobbyUID}/players`)
-    .set(playersSnapshotValue)
-    .then(() => {
-      logger.info("Lobby joined.");
-      return { lobbyUID };
-    })
-    .catch((error: Error) => {
-      // Re-throwing the error as an HttpsError so that the client gets
-      // the error details.
-      throw new HttpsError("unknown", error.message, error);
+    playersSnapshotValue.push({
+      email,
+      ready: false,
+      joinedAt: Date.now().toString(),
     });
-});
 
-exports.readyup = onCall(async (request) => {
-  // Checking that the user is authenticated.
-  if (!request.auth) {
-    // Throwing an HttpsError so that the client gets the error details.
-    throw new HttpsError(
-      "failed-precondition",
-      "The function must be " + "called while authenticated."
-    );
+    console.log(`New players: ${playersSnapshotValue}`);
+
+    getDatabase()
+      .ref(`lobbies/${lobbyUID}/players`)
+      .set(playersSnapshotValue)
+      .then(() => {
+        logger.info("Lobby joined.");
+        return { lobbyUID };
+      })
+      .catch((error: Error) => {
+        // Re-throwing the error as an HttpsError so that the client gets
+        // the error details.
+        throw new HttpsError("unknown", error.message, error);
+      });
   }
+);
 
-  const { lobbyUID, email } = request.data;
-
-  // Add new player to the lobby if they don't already exist
-
-  const players = await getDatabase().ref(`lobbies/${lobbyUID}/players`).get();
-
-  const playersSnapshotValue = players.val();
-
-  let areAllPlayersReady = true;
-
-  playersSnapshotValue.forEach((player: any) => {
-    if (player.email === email) {
-      player.ready = true;
+exports.readyup = onCall(
+  async (
+    request: CallableRequest<{
+      email: string;
+      lobbyUID: string;
+    }>
+  ) => {
+    // Checking that the user is authenticated.
+    if (!request.auth) {
+      // Throwing an HttpsError so that the client gets the error details.
+      throw new HttpsError(
+        "failed-precondition",
+        "The function must be " + "called while authenticated."
+      );
     }
 
-    if (player.ready === false) {
-      areAllPlayersReady = false;
-    }
-  });
+    const { lobbyUID, email } = request.data;
 
-  if (playersSnapshotValue.length >= 3 && areAllPlayersReady) {
-    try {
-      await createGame(lobbyUID);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+    // Add new player to the lobby if they don't already exist
 
-  getDatabase()
-    .ref(`lobbies/${lobbyUID}/players`)
-    .set(playersSnapshotValue)
-    .then(() => {
-      logger.info("Lobby joined.");
-      return { lobbyUID };
-    })
-    .catch((error: Error) => {
-      // Re-throwing the error as an HttpsError so that the client gets
-      // the error details.
-      throw new HttpsError("unknown", error.message, error);
+    const players = await getDatabase()
+      .ref(`lobbies/${lobbyUID}/players`)
+      .get();
+
+    const playersSnapshotValue = players.val();
+
+    let areAllPlayersReady = true;
+
+    playersSnapshotValue.forEach((player: any) => {
+      if (player.email === email) {
+        player.ready = true;
+      }
+
+      if (player.ready === false) {
+        areAllPlayersReady = false;
+      }
     });
-});
+
+    if (playersSnapshotValue.length >= 3 && areAllPlayersReady) {
+      try {
+        await createGame(lobbyUID);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    getDatabase()
+      .ref(`lobbies/${lobbyUID}/players`)
+      .set(playersSnapshotValue)
+      .then(() => {
+        logger.info("Lobby joined.");
+        return { lobbyUID };
+      })
+      .catch((error: Error) => {
+        // Re-throwing the error as an HttpsError so that the client gets
+        // the error details.
+        throw new HttpsError("unknown", error.message, error);
+      });
+  }
+);
